afd367d85be57d7c051a9ee1af6cacfb17b52f4b
d2250 <d22502517@gmail.com>
2024-07-18 23:34:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@
 BOT_TOKEN=("7377716858:AAHxlRlyDuM7DK7u6LogI91LzEqPpwgt9xI")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("Yourneeds")
+OWNER_USERNAME=("Zishu1")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
 ADMIN_IDS=("1640964213")
